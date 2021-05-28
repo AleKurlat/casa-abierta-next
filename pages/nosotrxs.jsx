8600485ff4c4e3c4a3a7b208001f41fef177eb2a
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-05-28 20:45:29 -0300

    nosotrxs

---
+++ pages/nosotrxs.jsx
@@ -0,0 +1,13 @@
+import Contacto from '../componentes/contacto.jsx';
+
+export default function Descripcion(props) {
+    return (
+        <div className="card2">
+            <h2 className="tituloCardGrande">Centro Cultural Casa Abierta</h2>
+            <h4 className="">Saavedra, C.A.B.A.</h4>
+            <div className="my-2">Un espacio de encuentro. Un lugar para compartir palabras, músicas, miradas. Una casa que abraza para formar redes. Un rincón en el barrio.</div>
+            <Contacto />
+            <img className="my-2" src="https://i.ibb.co/cNyzhy4/logo-Casa-Abierta.jpg" alt="Logo de Casa Abierta" />
+        </div>
+    )
+}
