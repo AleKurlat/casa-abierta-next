7e0adfd52df7f847efff7a8dfc4f6e4a904a1fca
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-05-29 20:11:01 -0300

    og type

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,7 @@ function MyApp({ Component, pageProps }) {
           <meta property="og:site_name" content="Centro Cultural Casa Abierta" />
           <meta name="twitter:title" content="Centro Cultural Casa Abierta" />
           <meta name="twitter:description" content="Centro Cultural Casa Abierta. Saavedra, C.A.B.A." />
+          <meta property="og:type" content="website" />
         </Head>
         <Component {...pageProps} />
       </Principal>
